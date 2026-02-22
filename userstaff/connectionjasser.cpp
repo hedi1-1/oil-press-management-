a961b28297db20ca3cf95ee50da33a1e90c0d654
xVamp99 <Jasser.Ouni@esprit.tn>
2026-02-22 19:05:53 +0100

    Integration branch - Jasser version

---
+++ userstaff/connectionjasser.cpp
@@ -0,0 +1,36 @@
+#include "connectionjasser.h"
+
+ConnectionJasser::ConnectionJasser() {
+  db = QSqlDatabase::addDatabase("QODBC", "jasser_conn");
+  db.setDatabaseName("Projet2A");
+  db.setUserName("zitouna");
+  db.setPassword("zitouna");
+}
+
+ConnectionJasser &ConnectionJasser::getInstance() {
+  static ConnectionJasser instance;
+  return instance;
+}
+
+bool ConnectionJasser::createconnect() {
+  if (db.isOpen()) {
+    return true;
+  }
+
+  if (db.open()) {
+    qDebug() << "[Jasser] Connexion à la base de données réussie!";
+    return true;
+  } else {
+    qDebug() << "[Jasser] Échec de connexion:" << db.lastError().text();
+    return false;
+  }
+}
+
+void ConnectionJasser::closeconnect() {
+  if (db.isOpen()) {
+    db.close();
+    qDebug() << "[Jasser] Connexion fermée.";
+  }
+}
+
+QSqlDatabase ConnectionJasser::getDatabase() { return db; }
--- userstaff/userstaff.pro
+++ userstaff/userstaff.pro
@@ -1,4 +1,4 @@
-QT       += core gui
+QT       += core gui sql
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
@@ -10,9 +10,11 @@ CONFIG += c++17
 
 SOURCES += \
     main.cpp \
+    connectionjasser.cpp \
     userstaff.cpp
 
 HEADERS += \
+    connectionjasser.h \
     userstaff.h
 
 FORMS += \
