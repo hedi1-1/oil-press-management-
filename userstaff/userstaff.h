9cf29efa24940c0bcebb3c0587520e2e213ae88c
hedi1-1 <triki.hedi@Esprit.tn>
2026-03-01 16:57:16 +0100

    feat(userstaff): implement full CRUD with add/edit/delete dialogs and PDF export

--- userstaff/userstaff.h
+++ userstaff/userstaff.h
@@ -2,6 +2,23 @@
 #define USERSTAFF_H
 
 #include <QMainWindow>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QMessageBox>
+#include <QDialog>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QFormLayout>
+#include <QDialogButtonBox>
+#include <QLabel>
+#include <QPushButton>
+#include <QCheckBox>
+#include <QPrinter>
+#include <QPainter>
+#include <QFileDialog>
+#include <QDateTime>
+#include <QCryptographicHash>
+#include <QTableWidgetItem>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -22,8 +39,26 @@ signals:
 
 private slots:
     void onBackClicked();
+    void onAjouterClicked();
+    void onModifierClicked();
+    void onSupprimerClicked();
+    void onExporterClicked();
+    void onTableSelectionChanged();
+    void onSearchTextChanged(const QString &text);
+    void onRoleFilterChanged(const QString &role);
+    void onShowArchivedToggled(bool checked);
 
 private:
     Ui::userstaff *ui;
+
+    void loadEmployees(const QString &searchText = "", const QString &roleFilter = "", bool showArchived = false);
+    void updateStats();
+    int  getSelectedUserId();
+    QString hashPassword(const QString &password);
+    QDialog* buildEmployeeDialog(const QString &title,
+                                 const QString &username = "",
+                                 const QString &email    = "",
+                                 const QString &role     = "Admin",
+                                 const QString &state    = "ACTIVE");
 };
 #endif // USERSTAFF_H
